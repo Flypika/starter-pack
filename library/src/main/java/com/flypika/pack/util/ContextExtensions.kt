7f68e8fde9a55dd7a94e382973f07135bca7ffa0
EvGem <evg200003@gmail.com>
2020-01-08 13:35:04 +0300

    Add startActivity extension

---
+++ library/src/main/java/com/flypika/pack/util/ContextExtensions.kt
@@ -0,0 +1,10 @@
+package com.flypika.pack.util
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+
+inline fun <reified A : AppCompatActivity> Context.startActivity(block: Intent.() -> Unit = {}) {
+    val intent = Intent(this, A::class.java).apply(block)
+    startActivity(intent)
+}
