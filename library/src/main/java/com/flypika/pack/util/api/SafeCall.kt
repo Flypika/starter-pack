26f08305e957f0bac6940eeb4c67caac7ae45fd6
EvGem <evg200003@gmail.com>
2020-01-08 19:16:17 +0300

    Add noResultSafeApiCall

--- library/src/main/java/com/flypika/pack/util/api/SafeCall.kt
+++ library/src/main/java/com/flypika/pack/util/api/SafeCall.kt
@@ -5,3 +5,12 @@ suspend inline fun <T> safeApiCall(crossinline block: suspend () -> T): ResultWr
 } catch (throwable: Throwable) {
     ResultWrapper.Failure(throwable)
 }
+
+suspend inline fun <T> noResultSafeApiCall(crossinline block: suspend () -> T): ResultWrapper<Unit> =
+    safeApiCall(block).let {
+        when (it) {
+            is ResultWrapper.Success -> ResultWrapper.Success(Unit)
+            is ResultWrapper.Failure -> it
+        }
+    }
+
