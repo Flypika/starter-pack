26e3cf028cfc68e6ca0f180ac1bf9591679bd54d
EvGem <evg200003@gmail.com>
2020-01-08 14:07:01 +0300

    Add coroutines

--- library/src/main/java/com/flypika/pack/ui/base/viewmodel/AuthViewModel.kt
+++ library/src/main/java/com/flypika/pack/ui/base/viewmodel/AuthViewModel.kt
@@ -1,17 +1,21 @@
 package com.flypika.pack.ui.base.viewmodel
 
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import retrofit2.HttpException
 
 abstract class AuthViewModel<A : ViewAction> : StarterViewModel<A>() {
 
-    protected inline fun request(block: () -> Unit) {
-        try {
-            block()
-        } catch (e: HttpException) {
-            if (e.code() == 401) {
-                onUnauthorized()
-            } else {
-                throw e
+    protected inline fun request(crossinline block: suspend () -> Unit) {
+        viewModelScope.launch {
+            try {
+                block()
+            } catch (e: HttpException) {
+                if (e.code() == 401) {
+                    onUnauthorized()
+                } else {
+                    throw e
+                }
             }
         }
     }
