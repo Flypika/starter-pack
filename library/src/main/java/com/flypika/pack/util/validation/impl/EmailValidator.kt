381ded0d3b7d9a254bedb409962c608f5d74b08f
EvGem <evg200003@gmail.com>
2020-01-24 17:01:03 +0300

    Change email validation

--- library/src/main/java/com/flypika/pack/util/validation/impl/EmailValidator.kt
+++ library/src/main/java/com/flypika/pack/util/validation/impl/EmailValidator.kt
@@ -1,10 +1,9 @@
 package com.flypika.pack.util.validation.impl
 
+import android.util.Patterns
 import com.flypika.pack.util.validation.Validator
 
 class EmailValidator : Validator {
 
-    private val regex = Regex("(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
-
-    override fun validate(string: String): Boolean = regex.matches(string)
+    override fun validate(string: String): Boolean = Patterns.EMAIL_ADDRESS.matcher(string).matches()
 }
