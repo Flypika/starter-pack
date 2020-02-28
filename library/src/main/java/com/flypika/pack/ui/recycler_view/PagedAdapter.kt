1efdd16724e622c50ecb1474282a6fe37deeac0e
EvGem <evg200003@gmail.com>
2020-02-28 19:23:47 +0300

    Fix attempt

--- library/src/main/java/com/flypika/pack/ui/recycler_view/PagedAdapter.kt
+++ library/src/main/java/com/flypika/pack/ui/recycler_view/PagedAdapter.kt
@@ -9,6 +9,7 @@ abstract class PagedAdapter<D, VH : RecyclerView.ViewHolder> : RecyclerView.Adap
         set(value) {
             field = value
             field?.attachAdapter(this)
+            notifyDataSetChanged()
         }
 
     override fun getItemCount(): Int = pagingListWrapper?.itemCount ?: 0
