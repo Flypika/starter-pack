4cc0f5673ed66fc02a5d77b0dc4b1db4076611ac
EvGem <evg200003@gmail.com>
2020-03-04 21:04:06 +0300

    [*] Move Diff Util logic to PagingListWrapper. Calculate diff on background thread

--- library/src/main/java/com/flypika/pack/ui/recycler_view/PagedAdapter.kt
+++ library/src/main/java/com/flypika/pack/ui/recycler_view/PagedAdapter.kt
@@ -1,24 +1,18 @@
 package com.flypika.pack.ui.recycler_view
 
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 
 abstract class PagedAdapter<D, VH : RecyclerView.ViewHolder> : RecyclerView.Adapter<VH>() {
 
     var pagingListWrapper: PagingListWrapper<D>? = null
         set(value) {
-            val oldData = field?.data
+            notifyItemRangeRemoved(0, itemCount)
+
             field?.detachAdapter()
 
             field = value
             field?.attachAdapter(this)
-
-            if (oldData != null) {
-                onNewData(oldData, emptyList())
-            }
         }
-
-    abstract fun onNewData(oldData: List<D>, newData: List<D>)
 
     override fun getItemCount(): Int = pagingListWrapper?.itemCount ?: 0
 
