33f1169247a9b04008cb3d87a5eacce002e016cd
Timur Seisembayev <seisembayev@strongte.am>
2020-07-09 22:24:03 +0600

    refactor: replaced SingleLiveEvent from java to kotlin

--- library/src/main/java/com/flypika/pack/presentation/base/SingleLiveEvent.kt
+++ library/src/main/java/com/flypika/pack/presentation/base/SingleLiveEvent.kt
@@ -14,66 +14,62 @@
  *  limitations under the License.
  */
 
-package com.flypika.pack.presentation.base;
+package com.flypika.pack.presentation.base
 
-import android.util.Log;
+import android.util.Log
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 
-import androidx.annotation.MainThread;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.lifecycle.LifecycleOwner;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.Observer;
-
-import java.util.concurrent.atomic.AtomicBoolean;
-
+import java.util.concurrent.atomic.AtomicBoolean
 
 /**
  * A lifecycle-aware observable that sends only new updates after subscription, used for events like
  * navigation and Snackbar messages.
- * <p>
+ *
+ *
  * This avoids a common problem with events: on configuration change (like rotation) an update
  * can be emitted if the observer is active. This LiveData only calls the observable if there's an
  * explicit call to setValue() or call().
- * <p>
+ *
+ *
  * Note that only one observer is going to be notified of changes.
  */
-public class SingleLiveEvent<T> extends MutableLiveData<T> {
+class SingleLiveEvent<T> : MutableLiveData<T>() {
 
-    private static final String TAG = "SingleLiveEvent";
+    companion object {
+        private const val TAG = "SingleLiveEvent"
+    }
 
-    private final AtomicBoolean mPending = new AtomicBoolean(false);
+    private val pending = AtomicBoolean(false)
 
     @MainThread
-    @Override
-    public void observe(@NonNull LifecycleOwner owner, @NonNull final Observer<? super T> observer) {
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
 
         if (hasActiveObservers()) {
-            Log.w(TAG, "Multiple observers registered but only one will be notified of changes.");
+            Log.w(TAG, "Multiple observers registered but only one will be notified of changes.")
         }
 
         // Observe the internal MutableLiveData
-        super.observe(owner, new Observer<T>() {
-            @Override
-            public void onChanged(@Nullable T t) {
-                if (mPending.compareAndSet(true, false)) {
-                    observer.onChanged(t);
-                }
+        super.observe(owner, Observer { t ->
+            if (pending.compareAndSet(true, false)) {
+                observer.onChanged(t)
             }
-        });
+        })
     }
 
     @MainThread
-    public void setValue(@Nullable T t) {
-        mPending.set(true);
-        super.setValue(t);
+    override fun setValue(t: T?) {
+        pending.set(true)
+        super.setValue(t)
     }
 
     /**
      * Used for cases where T is Void, to make calls cleaner.
      */
     @MainThread
-    public void call() {
-        setValue(null);
+    fun call() {
+        value = null
     }
 }
