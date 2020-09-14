a3ffc37351082247ed106f76ba396f533a2db7af
Timur Seisembayev <seisembayev@strongte.am>
2020-09-14 22:15:20 +0600

    refactor: deleted old adapter delegate

--- starter-pack/src/main/java/com/flypika/pack/presentation/base/adapter/model/IAdapterComparableItem.kt
+++ starter-pack/src/main/java/com/flypika/pack/presentation/base/adapter/model/IAdapterComparableItem.kt
@@ -1,21 +1,8 @@
 package com.flypika.pack.presentation.base.adapter.model
 
-/**
- * interface to be used with [DiffUtilCompositeAdapter], [PagingCompositeAdapter] items
- */
 interface IAdapterComparableItem {
 
-    /**
-     * [DiffUtilCallback] uses this method to distinguish between items
-     *
-     * @return item's unique id that won't be changed
-     */
     fun id(): Any
 
-    /**
-     * [DiffUtilCallback] uses this method to know, that item has changed
-     *
-     * @return data that represents visual part of the item
-     */
     fun content(): Any
 }
--- starter-pack/src/main/java/com/flypika/pack/presentation/base/adapter/model/IAdapterItem.kt
+++ starter-pack/src/main/java/com/flypika/pack/presentation/base/adapter/model/IAdapterItem.kt
@@ -1,6 +1,3 @@
 package com.flypika.pack.presentation.base.adapter.model
 
-/**
- * Empty interface to be used with [CompositeDelegateAdapter] items
- */
 interface IAdapterItem
